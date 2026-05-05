fd808df315d2867671b09ae781ed67fd19bb208e
Yu-Hsuan-1220 <Yu-Hsuan-1220>
2026-05-06 01:18:58 +0800

    update best run

--- HW3/best_run.sh
+++ HW3/best_run.sh
@@ -1,6 +1,6 @@
 python prepare_coco_dataset.py
 
-python train.py --fold 0 --epochs 50 --amp
+python train.py --fold 0 --epochs 100 --amp
 python train.py --fold 1 --epochs 50 --amp
 python train.py --fold 2 --epochs 50 --amp
 python train.py --fold 3 --epochs 50 --amp
@@ -12,4 +12,4 @@ python inference.py \
                 ../work_dirs/fold2/best_coco_segm_mAP_50_epoch_45.pth \
                 ../work_dirs/fold3/best_coco_segm_mAP_50_epoch_30.pth \
                 ../work_dirs/fold4/best_coco_segm_mAP_50_epoch_40.pth \
-  --output ../test-results-ensemble.json --tta --load_all_models
+  --output ../test-results-ensemble.json --tta --load_all_models --tta_rotation
